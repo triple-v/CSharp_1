6de6715682206a1ce187dc42b667bebe6ca03216
Vladislav <vasilevich.account@yandex.ru>
2022-10-02 21:53:54 +0300

    Выполнено третье задание

---
+++ Task_3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число:");
+int num=Convert.ToInt32(Console.ReadLine());
+if(num%2==0){
+  Console.WriteLine($"Число {num} является чётным.");
+}
+else{
+  Console.WriteLine($"Число {num} является нечётным.");
+}
