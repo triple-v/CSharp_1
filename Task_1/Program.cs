120aa6e87f09e5c245709160d7c0a4b4fa2ef080
Vladislav <vasilevich.account@yandex.ru>
2022-10-02 21:32:00 +0300

    Выполнено первое задание

---
+++ Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое целое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число:");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a == b){
+    Console.WriteLine($"{a} = {b} (min и max отсутствуют)");
+}
+else{
+    if(a>b){
+    Console.WriteLine($"{a} > {b} (min = {b}; max = {a})");
+    }
+    else{
+        Console.WriteLine($"{a} < {b} (min = {a}; max = {b})");
+    }
+}
